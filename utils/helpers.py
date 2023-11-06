e070d7501af667432c807c4dd0c938a62e16055a
cohenaj194 <cohenaj194@gmail.com>
2023-11-06 10:55:00 -0500

    Add option to remove russian realms

--- utils/helpers.py
+++ utils/helpers.py
@@ -14,3 +14,7 @@ def create_oribos_exchange_item_link(realm_name, item_id, region):
     else:
         url_region = "eu"
     return f"https://oribos.exchange/#{url_region}-{fixed_realm_name}/{item_id}"
+
+
+def get_wow_russian_realm_ids():
+    return [1602, 1604, 1605, 1614, 1615, 1623, 1923, 1925, 1928, 1929, 1922]
